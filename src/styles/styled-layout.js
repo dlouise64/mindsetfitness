1308cbb45de218cd38066fc5914e3d4d46688a83
dlouise64 <dana.iti.work@gmail.com>
2020-01-25 18:35:26 +1300

    mobile first

--- src/styles/styled-layout.js
+++ src/styles/styled-layout.js
@@ -1,7 +1,11 @@
 import styled from '@emotion/styled'
 
 export const Container = styled.div`
-	margin: 0 25%;
+	margin: 0 5%;
+
+	@media (min-width: 1024px) {
+		margin: 0 25%;
+	}
 `
 export const Section = styled.section`
 	padding: 3.125rem 0;
