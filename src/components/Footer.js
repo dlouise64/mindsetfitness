09acc2a6ed40a12623b1fa3050ccd62bbda2945a
dlouise64 <dana.iti.work@gmail.com>
2020-01-25 18:39:54 +1300

    add columns

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,12 +4,26 @@ import Theme from '../styles/theme'
 import { Container } from '../styles/styled-layout'
 const Footer = () => (
 	<FooterWrapper>
-		<Container>Footer</Container>
+		<Container>
+			<Columns>
+				<div>Left</div>
+				<div>right</div>
+			</Columns>
+		</Container>
 	</FooterWrapper>
 )
 
 const FooterWrapper = styled.footer`
 	padding: 2.8125rem 0;
 	background-color: ${Theme.colors.dark};
+`
+
+const Columns = styled.div`
+	display: flex;
+	align-items: center;
+
+	> div {
+		flex: 1;
+	}
 `
 export default Footer
