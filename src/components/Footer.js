815b679ccf0e774296b1870d4339cd8738d0ac09
dlouise64 <dana.iti.work@gmail.com>
2020-01-25 18:44:36 +1300

    add content to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,33 @@ const Footer = () => (
 	<FooterWrapper>
 		<Container>
 			<Columns>
-				<div>Left</div>
-				<div>right</div>
+				<div>
+					<h3>Challenge the mind and the body will follow.</h3>
+					<p>Connect with us:</p>
+					<ul>
+						<li>facebook</li>
+						<li>twitter</li>
+						<li>linkedin</li>
+						<li>instagram</li>
+						<li>youtube</li>
+					</ul>
+				</div>
+				<div>
+					<p>
+						Subscribe to Mindset Fitness and receive free tips, recipes, updates
+						and more.
+					</p>
+					<div>Form</div>
+				</div>
+				<nav>
+					<ul>
+						<li>About</li>
+						<li>Team</li>
+						<li>Services</li>
+						<li>Testimonials</li>
+						<li>Contact</li>
+					</ul>
+				</nav>
 			</Columns>
 		</Container>
 	</FooterWrapper>
